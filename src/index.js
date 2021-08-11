6eadeaf67b1062683a343f7ca6c48649f4d19b2b
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-08-11 22:22:52 +0900

    WiP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-const core = require("@actions/core");
+const core = require('@actions/core');
+require('dotenv').config();
+const { Client } = require('@notionhq/client');
+
+const NOTION_API_KEY = process.env.NOTION_API_KEY;
+const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 const main = () => {
-  
+  console.log(DATABASE_ID);
+  console.log(NOTION_API_KEY);
 }
 
 const getPullRequestBody = () => {
