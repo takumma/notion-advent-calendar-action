72c7d4e2acda3293d24b58825e46365c99cc1a5b
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-08-11 22:01:47 +0900

    Update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const core = require("@actions/core");
+
+const main = () => {
+  
+}
+
+const getPullRequestBody = () => {
+  const eventPayload = require(process.env.GITHUB_EVENT_PATH)
+}
+
+main();
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-
-const main = () => {
-  
-}
-
-main();
