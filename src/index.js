ed052481cf2eaa8733f218abb39dfee32dc4eba0
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-08-23 22:59:10 +0900

    Fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const { Client } = require("@notionhq/client")
-// // envs for develop
-// require('dotenv').config()
 
 const NOTION_TOKEN = process.env.NOTION_TOKEN;
 const DATABASE_ID = process.env.NOTION_DATABASE_ID;
@@ -9,89 +7,62 @@ const notion = new Client({ auth: NOTION_TOKEN});
 
 const main = () => {
   const eventPayload = require(process.env.GITHUB_EVENT_PATH);
-  const body = eventPayload.pull_request.body; // process.env.pull_request_body
-  const { title, tags, url, date } = getPropertiesFromBody(body);
-  addArticle({title: title, tags: tags, url: url, date: date});
+  const body = eventPayload.pull_request.body;
+  const properties = getPropertiesFromBody(body);
+  addArticle(properties);
 }
 
 const getPropertiesFromBody = (body) => {
-  const result = {
-    title: "",
-    tags: [],
-    url: "",
-    date: "",
-  }
   const propertyTexts = body.split('\r\n');
+  const result = {}
   for(let propertyText of propertyTexts) {
     const [name, ...props] = propertyText.split(' ');
-    console.log(name)
-    console.log(props)
     switch (name) {
       case '/title':
-        result.title = props.join(' ');
+        result["title"] = {
+          title: [
+            {
+              text: {
+                content: props.join(' '),
+              },
+            },
+          ],
+        };
         break;
       case '/tags':
-        result.tags = props;
+        result["Tags"] = {
+          type: "multi_select",
+          multi_select: props.map((tag) => {
+            return {
+              name: tag
+            };
+          })
+        };
         break;
       case '/url':
-        result.url = props.join();
+        result["URL"] = {
+          type: "url",
+          url: props.join(),
+        };
         break;
       case '/date':
-        result.date = props.join();
+        result["Date"] = {
+          type: "date",
+          date: {
+            start: props.join(),
+            end: null,
+          }
+        };
     }
   }
   return result
 }
 
-const getArticles = async () => {
-  try {
-    const resp = await notion.databases.query({
-      database_id: DATABASE_ID,
-    });
-  } catch (e) {
-    console.error(e);
-  }
-}
-
-const addArticle = async ({
-  title,
-  tags,
-  url,
-  date
-}) => {
+const addArticle = async (properties) => {
   try {
     const resp = await notion.pages.create({
       parent: { database_id: DATABASE_ID },
-      properties: {
-        title: {
-          title: [
-            {
-              text: {
-                content: title,
-              },
-            },
-          ],
-        },
-        Date: {
-          type: "date",
-          date: {
-            start: date,
-            end: null,
-          }
-        },
-        Tags: {
-          type: "multi_select",
-          multi_select: tags.map((tag) => {
-            return {
-              name: tag
-            };
-          })
-        },
-        URL: {
-          type: "url",
-          url: url,
-        }
-      },
+      properties: { ...properties },
     })
     console.log(resp);
   } catch (e) {
